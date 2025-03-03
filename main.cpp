4480063d7aebad3ca92112941a0d7ee7f35f27fd
Vany188 <ttrr73339@gmail.com>
2025-03-03 20:00:04 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int a{5}, b{7};
+    cout << "input two number" << endl;
+    cout<<"first number"<<endl;
+    cout<<"a+b="<<a+b<<endl;
+    return 0;
+}
