edc5196b7b25609a28044899ec725b5fcf794fc2
Vany188 <ttrr73339@gmail.com>
2025-03-31 20:13:06 +0300

    Add files via upload

---
+++ 31.03.25 n2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+float y(int n){
+    cout <<"введите количество сторон многоугольника:"<<endl;
+    cin >>n;
+    cout<<"сумма углов многоугольника:"<<endl;
+    return ((n-2)*180);
+}
+float v(int n1,int n2,int n3){
+    cout <<"введите стороны прямоугольника:"<<endl;
+    cin >>n1;
+    cin >>n2;
+    cin >>n3;
+    if(n3==0||n2==0||n1==0)
+    {
+        if(n1*n2!=0||n2*n3!=0||n1*n3!=0)
+        {
+            cout<<"площадь прямоугольника:"<<endl;
+            return(n1*n2+n2*n3+n3*n1);
+        }
+    }
+    cout<<"обьем равен:"<<endl;
+    return n1*n2*n3;
+}
+int u(int t){
+    cout<<"введите количество отрезков"<<endl;
+    int x,g,n;
+    cin>>n;
+    g=0;
+    for(int i=1;i<n+1;i++){
+        cin>> x;
+        g=g+x;
+    }
+    cout<<"длинна ломаной:"<<endl;
+    return (g);
+}
+
+int main() {
+    cout<<"что вы хотити сделать:"<<endl;
+    cout<<"1 нахождение площадь прямоугольника"<<endl;
+    cout<<"2 нахождение обьема паралепипида"<<endl;
+    cout<<"3 сумма углов многоугольника"<<endl;
+    cout<<"найти длинну ломаной"<<endl;
+    cout<<"введите номер пункта"<<endl;
+    int n;
+    cin>>n;
+    if(n==1)
+    {
+        cout<< "нахождение площади прямоугольника"<<endl;
+        int a,b,c;
+        cout<<v(a,b,c)<<endl;
+    }
+    if(n==2)
+    {
+        cout<< "нахождение обьема паралепипида"<<endl;
+        int a,b,c;
+        cout<<v(a,b,c)<<endl;
+    }
+    if(n==3)
+    {
+        cout<< "сумма углов многоугольника"<<endl;
+        int a;
+        cout<<y(a)<<endl;
+    }
+    if(n==4)
+    {
+        int y;
+        cout<<u(y)<<endl;
+    }
+    return 0;
+}
